8a4685c64cd85056c2d54518bba6f7d0bbec9cc4
ivabrunec <iva.brunec@gmail.com>
2024-09-24 13:52:13 -0400

    update 9/24

--- 2024/2024_09_24/math_olympiad.R
+++ 2024/2024_09_24/math_olympiad.R
@@ -3,6 +3,10 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 library(dplyr)
 library(ggplot2)
+library(showtext)
+
+font_add_google(name = 'Roboto Mono', family = 'roboto')
+showtext_auto()
 
 tuesdata <- tidytuesdayR::tt_load('2024-09-24')
 
@@ -25,17 +29,23 @@ ggplot(data = timeline_df) +
                 ymax = ratio, xmax = year+.4),
             fill='coral') +
   geom_point(aes(y = segment_male_start, x = year),
-             size = 1, color = 'grey75') +
+             size = 2, color = 'grey75') +
   geom_point(aes(y = ratio, x = year),
-             size = 1, color = 'coral') +
-  geom_text(aes(y = 0, x = year,
-                label = (round(ratio,2))*100),
-            vjust = 1.5, size = 8, color = 'black') +
+             size = 2, color = 'coral') +
+  #geom_text(aes(y = 0, x = year,
+  #              label = (round(ratio,2))*100),
+  #          vjust = 1.5, size = 8, color = 'black', family = 'roboto') +
+  scale_y_continuous(breaks = c(0, .05, .1, .15, .2)) + 
   theme_minimal() +
-  theme(axis.text.y = element_blank(),
-        axis.text.x = element_text(size = 40),
+  theme(axis.text.y = element_text(size = 20, family = 'roboto', color = 'grey20'),
+        axis.text.x = element_text(size = 30, family = 'roboto'),
         axis.title = element_blank(),
-        plot.background = element_rect(fill = 'grey90', color = NA))
+        plot.background = element_rect(fill = 'grey90', color = NA),
+        plot.title = element_text(color = 'black', 
+                                  size = 35, 
+                                  family = 'roboto',
+                                  hjust = .5)) +
+  labs(title = 'Share of female contestants in the International Mathematical Olympiad, 1959-2024')
 
-ggsave('temp.png', height = 6, width = 8)
+ggsave('math_olympiad.png', height = 6, width = 8)
 
